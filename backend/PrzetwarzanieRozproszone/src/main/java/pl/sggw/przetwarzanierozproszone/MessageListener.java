8a1a140175a5aaadbcf73b5c46d2ff9c941e6eae
Filip Rosochacki <63207978+simplymisfit@users.noreply.github.com>
2023-05-06 13:11:36 +0200

    Added initial MQRabbit integration

---
+++ backend/PrzetwarzanieRozproszone/src/main/java/pl/sggw/przetwarzanierozproszone/MessageListener.java
@@ -0,0 +1,12 @@
+package pl.sggw.przetwarzanierozproszone;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageListener {
+    @RabbitListener(queues = MQConfig.QUEUE)
+    public void listener(CustomMessage message){
+        System.out.println(message);
+    }
+}
