ffa19d85192f22172b7b0e2584b8afa200a1594b
pawelrogulski <pawelrogulski@o2.pl>
2023-05-06 12:10:07 +0200

    Initial Commit

---
+++ backend/PrzetwarzanieRozproszone/src/main/java/pl/sggw/przetwarzanierozproszone/PrzetwarzanieRozproszoneApplication.java
@@ -0,0 +1,13 @@
+package pl.sggw.przetwarzanierozproszone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrzetwarzanieRozproszoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrzetwarzanieRozproszoneApplication.class, args);
+    }
+
+}
---
+++ backend/PrzetwarzanieRozproszone/src/test/java/pl/sggw/przetwarzanierozproszone/PrzetwarzanieRozproszoneApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sggw.przetwarzanierozproszone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrzetwarzanieRozproszoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
