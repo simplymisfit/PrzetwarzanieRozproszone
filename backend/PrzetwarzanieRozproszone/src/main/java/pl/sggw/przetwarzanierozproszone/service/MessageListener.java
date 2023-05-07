485da07283c1c2550882f2a9b44b5abe56bb7f2b
pawelrogulski <pawelrogulski@o2.pl>
2023-05-07 15:56:00 +0200

    Global chat auth

--- backend/PrzetwarzanieRozproszone/src/main/java/pl/sggw/przetwarzanierozproszone/service/MessageListener.java
+++ backend/PrzetwarzanieRozproszone/src/main/java/pl/sggw/przetwarzanierozproszone/service/MessageListener.java
@@ -7,8 +7,9 @@ import pl.sggw.przetwarzanierozproszone.domain.CustomMessage;
 
 @Component
 public class MessageListener {
+
     @RabbitListener(queues = MQConfig.QUEUE)
-    public void listener(CustomMessage message){
-        System.out.println(message);
+    public void chatListener(CustomMessage message){
+        System.out.println(message.getMessageDate().getHours() + ":" + message.getMessageDate().getMinutes() + "\t" + message.getPlayerName() + " : " + message.getMessage());
     }
 }
---
+++ backend/PrzetwarzanieRozproszone/src/main/java/pl/sggw/przetwarzanierozproszone/service/MessagePublisher.java
@@ -0,0 +1,28 @@
+package pl.sggw.przetwarzanierozproszone.service;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import pl.sggw.przetwarzanierozproszone.configuration.MQConfig;
+import pl.sggw.przetwarzanierozproszone.domain.CustomMessage;
+
+import java.util.Date;
+import java.util.UUID;
+
+@RestController
+public class MessagePublisher {
+
+    @Autowired
+    private RabbitTemplate template;
+
+
+    @PostMapping("/chat")
+    public void sendChatMessage(@RequestBody CustomMessage message){
+        message.setMessageId(UUID.randomUUID().toString());
+        message.setMessageDate(new Date());
+        template.convertAndSend(MQConfig.EXCHANGE, MQConfig.ROUTING_KEY, message);
+    }
+
+}
