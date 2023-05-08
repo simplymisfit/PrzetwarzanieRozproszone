3cb982407e6471f25e628bee8a0dfcecd84a0ceb
Adam <adam.kowalczuk@poczta.fm>
2023-05-08 22:07:42 +0200

    Setting the project structure

---
+++ front/src/styles/theme.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  colors: {
+    red: "#FB1B1B",
+    darkBlue: "#0A285F",
+    black: "#000000",
+    blue: "#0075BE",
+    gray: "#DFDFDF",
+    orange: "#D5A100",
+    white: "#FFFFFF",
+    yellow: "#FFCC00",
+  },
+};
