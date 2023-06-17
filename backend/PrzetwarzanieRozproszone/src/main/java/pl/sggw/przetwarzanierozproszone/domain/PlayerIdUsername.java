88ca25c3c6f338554d23f5a8650fb7e157447fe7
pawelrogulski <pawelrogulski@o2.pl>
2023-06-17 16:55:21 +0200

    Bugfix BeanSerializer

--- backend/PrzetwarzanieRozproszone/src/main/java/pl/sggw/przetwarzanierozproszone/domain/PlayerIdUsername.java
+++ backend/PrzetwarzanieRozproszone/src/main/java/pl/sggw/przetwarzanierozproszone/domain/PlayerIdUsername.java
@@ -2,8 +2,10 @@ package pl.sggw.przetwarzanierozproszone.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 public class PlayerIdUsername {
     private int id;
     private String username;
